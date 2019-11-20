b7a64e787d98396272e0f265b3797117bcd5bb1d
League Student <league@iMac-27.attlocal.net>
2019-11-19 18:27:01 -0800

    ObjectManager
    Still 2/3(unproductive day):confused:

--- src/ObjectManager.java
+++ src/ObjectManager.java
@@ -1,4 +1,62 @@
+import java.awt.Graphics;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class ObjectManager {
+	Rocketship r;
+	ArrayList<Projectile> projectiles = new ArrayList<Projectile>();
+	ArrayList<Alien> aliens = new ArrayList<Alien>();
+	Random random = new Random();
 
+	void addProjectile(Projectile p){
+		projectiles.add(p);
+		
+	}
+	
+	void addAlien(){
+		aliens.add(new Alien(random.nextInt(LeagueInvaders.WIDTH),0,50,50));
+		
+	}
+	
+	void update() {
+		for(int i = 0; i < aliens.size(); i++) {
+			aliens.get(i).update();
+			if(aliens.get(i).y>800) {
+				aliens.get(i).isActive = false;
+			}
+		}
+		
+		for(int i = 0; i < projectiles.size(); i++) {
+			projectiles.get(i).update();
+			if(projectiles.get(i).y<0) {
+				projectiles.get(i).isActive = false;
+			}
+		}
+	}
+	
+	void draw(Graphics g){
+		r.draw(g);
+		
+		for(int i = 0; i < aliens.size(); i++) {
+			aliens.get(i).draw(g);
+		}
+		
+		for(int i = 0; i < projectiles.size(); i++) {
+			projectiles.get(i).draw(g);
+		}
+	}
+	
+	void purgeObjects(){
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
